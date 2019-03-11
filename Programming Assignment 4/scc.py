4a1804222d92dfcd4093d8a27e04ae635b2ad64c
Robert Senseman <robert.senseman@oculus.com>
2019-03-11 12:00:02 -0700

    iterative established

--- Programming Assignment 4/scc.py
+++ Programming Assignment 4/scc.py
@@ -1,77 +1,100 @@
 import sys
-from random import sample
 
 
 class dfs_loop():
     def __init__(self, graph, search_order=None):
-        self.depth = 0
         self.graph = graph
-        self.t = 0  # first pass only
         self.s = None  # second pass only
-        self.search_order = search_order if search_order else sample(list(range(1, len(graph) + 1)), len(graph))  # nodes ordered 1 to n
-        # self.search_order = search_order if search_order else list(range(1, len(graph) + 1))  # nodes ordered 1 to n
+        self.search_order = search_order if search_order else list(range(1, len(graph) + 1))  # nodes ordered 1 to n
         # print(f'search order: {self.search_order}')
         self.nodes_explored = set()
+        self.priority_stack = []
 
         self.leaders = [None] * (len(graph) + 1)
-        self.nodes_by_finishing_time = []
+        self.nodes_by_finishing_time = [] # replacemnt for variable t, only used for first pass
 
         self.loop()
 
     def loop(self):
+        ''' recursive approach deprecated in favor of iterative approach'''
+        # self.loop_recursive()
+        self.loop_iterative()
+
+    def loop_iterative(self):
+        '''implement iterative dfs to avoid stack overflow. Use a stack
+        seperate from search_order to prioritize exploration of node children
+        while maintaining record keeping of leaders
+        '''
+        while self.search_order or self.priority_stack:
+            if self.priority_stack:
+                node = self.priority_stack.pop()
+                if node not in self.nodes_explored:
+                    self.dfs_iterative(node)
+            else:
+                node = self.search_order.pop()
+                if node not in self.nodes_explored:
+                    self.s = node
+                    self.dfs_iterative(node)
+
+    def dfs_iterative(self, node):
+        self.nodes_explored.add(node)
+        self.leaders[node] = self.s
+
+        for arc in self.graph[node]:
+            if arc not in self.nodes_explored:
+                self.priority_stack.append(arc)
+        self.nodes_by_finishing_time.append(node)
+
+    def loop_recursive(self):
         while self.search_order:
             node = self.search_order.pop()
             if node not in self.nodes_explored:
                 self.s = node
-                self.dfs(node)
+                self.dfs_recursive(node)
 
-    def dfs(self, node):
+    def dfs_recursive(self, node):
         self.nodes_explored.add(node)
         self.leaders[node] = self.s
 
-        # print(self.depth)
         for arc in self.graph[node]:
             if arc not in self.nodes_explored:
-                self.depth += 1
-                self.dfs(arc)
-                self.depth -= 1
-        self.t += 1
+                self.dfs_recursive(arc)
         self.nodes_by_finishing_time.append(node)
 
 
 class graph():
     def __init__(self, pair_list):
         self.graph = {}
-        self.graph_reverse = {}
 
         self._build_graph(pair_list)
-
 
     def _build_graph(self, pair_list):
         for x, y in pair_list:
             if x in self.graph:
                 self.graph[x]['outgoing'].append(y)
             else:
-                self.graph[x] = {'outgoing':[y], 'incoming':[]}
+                self.graph[x] = {'outgoing': [y], 'incoming': []}
 
             if y in self.graph:
                 self.graph[y]['incoming'].append(x)
             else:
-                self.graph[y] = {'outgoing':[], 'incoming':[x]}
-        print(self.graph[1])
+                self.graph[y] = {'outgoing': [], 'incoming': [x]}
 
     def find_strongly_connected_components(self):
-        print('starting first loop...')
         print(f'{max(self.graph)} elements')
-        first_loop = dfs_loop({k:self.graph[k]['incoming'] for k in self.graph.keys()})
+        print('starting first loop...')
+        first_loop = dfs_loop(
+            {k: self.graph[k]['incoming'] for k in self.graph.keys()}
+        )
         # print(f'finishing order: {first_loop.nodes_by_finishing_time}')
-        # print(f'First Loop Leaders: {first_loop.leaders}')
         print('starting second loop...')
         second_loop = dfs_loop(
-            {k:self.graph[k]['outgoing'] for k in self.graph.keys()},
+            {k: self.graph[k]['outgoing'] for k in self.graph.keys()},
             first_loop.nodes_by_finishing_time
         )
+        # print(f'Second Loop Leaders: {first_loop.leaders}')
         return second_loop.leaders
+
 
 def count_occurences(list_in):
     counter = [0] * (len(list_in) + 1)
@@ -81,9 +104,10 @@ def count_occurences(list_in):
 
     return sorted(counter, reverse=True)
 
+
 def make_pair_from_row(row):
-    # apply this to a generator?
     return [int(val) for val in row.strip().split(' ')]
+
 
 if __name__ == '__main__':
     filename = sys.argv[1]
@@ -93,7 +117,6 @@ if __name__ == '__main__':
 
     g = graph(pair_list)
     leaders = g.find_strongly_connected_components()
-    # leaders = leaders[1:]
     print(f'Second Loop Leaders: {leaders}')
     largest_sccs = count_occurences(leaders)
     print(largest_sccs[:10])
